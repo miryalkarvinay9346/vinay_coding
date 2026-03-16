8874aa71506f1213fd9b53f19da1169d34666d49
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-03-16 18:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (49.04%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.py
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         c=0
-        for i in range(1,max(a,b)+1):
+        for i in range(1,min(a,b)+1): #we can also use max(a,b) but it take more time than min(a,b) , since common values are found before(<=) the min value
             if(a%i==0 and b%i==0):
                 c+=1
         return c        
