69dca6a6f2290b24b59ea7299c412675ed8bc5a7
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-03-17 21:45:46 +0530

    Time: 5 ms (7.12%) | Memory: 12.4 MB (78.48%) - LeetSync

---
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        #if num==t :
+         #   return 0
+        return num+2*t    
+       
+
+
+        
