f52197e36df857f7cd4c57a15cc824ccf69af107
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-05-19 15:50:28 +0530

    Time: 4 ms (40.90%) | Memory: 12.7 MB (4.63%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        counts=Counter(nums1)&Counter(nums2)
+        return list(counts.elements())
+        
